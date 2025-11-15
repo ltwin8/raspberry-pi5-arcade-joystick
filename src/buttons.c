2eeb7608b34bc4124fd726d0313b83e2498dbb7a
ltwin8 <leon.osterhues@icloud.com>
2025-11-15 17:01:33 +0100

    Update button mappings for GPIO and BTN constants

--- src/buttons.c
+++ src/buttons.c
@@ -11,8 +11,8 @@ struct button_map buttons[] = {
     {GPIO_Y, BTN_Y},
     {GPIO_L1, BTN_TL},
     {GPIO_R1, BTN_TR},
-    {GPIO_GO, BTN_START},
-    {GPIO_ENTER, BTN_SELECT}
+    {GPIO_GO, BTN_ENTER},
+    {GPIO_ENTER, BTN_ESCAPE}
 };
 
 const int NUM_BUTTONS = sizeof(buttons) / sizeof(buttons[0]);
