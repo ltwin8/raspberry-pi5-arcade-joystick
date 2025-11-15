31420e22b2260a7daf0e4be765739f382006da36
ltwin8 <leon.osterhues@icloud.com>
2025-11-15 15:44:52 +0100

    Update GPIO pin definitions for buttons

--- src/include/buttons.h
+++ src/include/buttons.h
@@ -4,16 +4,18 @@
 #include <linux/uinput.h>
 
 // GPIO pin definitions for buttons
-#define GPIO_UP 17
-#define GPIO_DOWN 27
-#define GPIO_LEFT 22
-#define GPIO_RIGHT 23
-#define GPIO_A 5
-#define GPIO_B 6
-#define GPIO_X 13
-#define GPIO_Y 19
-#define GPIO_L1 26
-#define GPIO_R1 21
+#define GPIO_UP 5
+#define GPIO_DOWN 6
+#define GPIO_LEFT 13
+#define GPIO_RIGHT 19
+#define GPIO_A 26
+#define GPIO_B 12
+#define GPIO_X 16
+#define GPIO_Y 20
+#define GPIO_L1 18
+#define GPIO_R1 23
+#define GPIO_GO 21
+#define GPIO_ENTER 4
 
 // Constants for axis limits and polling
 #define AXIS_MIN 0
